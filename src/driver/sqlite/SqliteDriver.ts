3fb75e8dd3315d07c25a63585bfbb555aca995fc
Jurriaan Barkey Wolf <jjhbarkeywolf@gmail.com>
2020-09-17 17:55:27 +0200

    Remove patronizing DriverPackageNotInstalledError when sqlite can't be imported

--- src/driver/sqlite/SqliteDriver.ts
+++ src/driver/sqlite/SqliteDriver.ts
@@ -1,6 +1,5 @@
 import mkdirp from 'mkdirp';
 import path from 'path';
-import { DriverPackageNotInstalledError } from "../../error/DriverPackageNotInstalledError";
 import { SqliteQueryRunner } from "./SqliteQueryRunner";
 import { DriverOptionNotSetError } from "../../error/DriverOptionNotSetError";
 import { PlatformTools } from "../../platform/PlatformTools";
@@ -128,12 +127,7 @@ export class SqliteDriver extends AbstractSqliteDriver {
      * If driver dependency is not given explicitly, then try to load it via "require".
      */
     protected loadDependencies(): void {
-        try {
-            this.sqlite = PlatformTools.load("sqlite3").verbose();
-
-        } catch (e) {
-            throw new DriverPackageNotInstalledError("SQLite", "sqlite3");
-        }
+        this.sqlite = PlatformTools.load("sqlite3").verbose();
     }
 
     /**
