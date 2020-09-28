e47cb64ede6016d1bf1886627a3683b91d3ce1b2
Jurriaan Barkey Wolf <jjhbarkeywolf@gmail.com>
2020-09-28 11:10:48 +0200

    Return an empty array instead of throwing, as these functions may be invoked on connection initialization

--- src/util/DirectoryExportedClassesLoader.ts
+++ src/util/DirectoryExportedClassesLoader.ts
@@ -3,12 +3,14 @@ import {Logger} from "../logger/Logger";
  * Loads all exported classes from the given directory.
  */
 export function importClassesFromDirectories(logger: Logger, directories: string[], formats = [".js", ".cjs", ".ts"]): Function[] {
-    throw new Error("Function has been removed due to incompatibility with Webpack (used dynamic require())")
+    // Function has been removed due to incompatibility with Webpack (used dynamic require())
+    return []
 }
 
 /**
  * Loads all json files from the given directory.
  */
 export function importJsonsFromDirectories(directories: string[], format = ".json"): any[] {
-    throw new Error("Function has been removed due to incompatibility with Webpack (used dynamic require())")
+    // "Function has been removed due to incompatibility with Webpack (used dynamic require())"
+    return []
 }
